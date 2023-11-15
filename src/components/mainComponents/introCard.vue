d4ced9bec7c683dcdcf7cd83ff06c4a91348a836
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-15 19:05:32 +0100

    ADD - Database content + intro section + card comp

---
+++ src/components/mainComponents/introCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <img class="card-img" :src="card.iconPath">
+        <h3 class="card-title">{{ card.cardTitle }}</h3>
+        <p class="card-content">{{ card.cardText }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+}
+
+// .card-img{
+
+// }
+
+// .card-title{
+
+// }
+
+// .card-content{
+
+// }
+</style>
--- src/components/mainComponents/mainIntro.vue
+++ src/components/mainComponents/mainIntro.vue
@@ -1,10 +1,73 @@
 <script>
+import database from '../../../db.json'
+import card from './introCard.vue'
 export default {
-
+    components: {
+        card
+    },
+    data() {
+        return {
+            database
+        }
+    },
+    computed: {
+        cards() {
+            return this.database.cards
+        }
+    },
+    mounted() {
+        console.log(this.database.cards)
+    }
 }
 
 </script>
 
-<template></template>
+<template>
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="section-content">
+                    <h2 class="title">Welcome to Avada Health</h2>
+                    <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam eius
+                        placeat sequi iure repellat
+                        delectus fuga soluta quo quaerat dolores.</p>
+                </div>
+                <div class="cards">
+                    <card v-for="(card, index) in cards" :key="index" :card="card" />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use'../../style/partials/variables' as *;
+
+.cards {
+    display: flex;
+}
+
+.section-content {
+    text-align: center;
+}
+
+.section {
+    padding: 60px 0;
+}
+
+.container {
+    max-width: 1100px;
+    margin: 0 auto;
+    height: 100%;
+}
+
+.row {
+    display: flex;
+    flex-direction: column;
+    row-gap: 30px;
+}
+
+.title {}
+
+.dscription {}
+</style>
