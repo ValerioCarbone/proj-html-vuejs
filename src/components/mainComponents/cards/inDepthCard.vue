e740fec2b25584742ec1e7a4424a0382428724a1
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-20 16:56:52 +0100

    CHANGE - Refactory inDepth sec.

---
+++ src/components/mainComponents/cards/inDepthCard.vue
@@ -0,0 +1,100 @@
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="card col-4 white" :class="card.bgColor">
+        <h3 class="title">{{ card.cardTitle }}</h3>
+        <img src="/wave-divider.png">
+        <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga vel provident ea facere
+            nesciunt blanditiis asperiores.</p>
+        <a href="#" class="btn" :class="card.btnStyle">Learn More</a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../style/partials/variables' as *;
+
+button {
+    all: unset;
+}
+
+.card {
+    padding: 80px 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 30px;
+}
+
+.row {
+    display: flex;
+}
+
+.col-4 {
+    flex-basis: calc(100% / 3);
+}
+
+.white {
+    color: white;
+}
+
+.btn {
+    padding: 16px 32px;
+    border: 3px solid white;
+    text-transform: uppercase;
+    font-size: 18px;
+    letter-spacing: 2px;
+}
+
+.btn-lb:hover {
+    border: 3px solid $bg-blue;
+    color: $bg-blue;
+    background-color: white;
+}
+
+.btn-b:hover {
+    border: 3px solid $bg-dark-blue;
+    color: $bg-dark-blue;
+    background-color: white;
+}
+
+.btn-db:hover {
+    border: 3px solid $light-blue;
+    color: $light-blue;
+    background-color: white;
+}
+
+.bg-light-blue {
+    background-color: $light-blue;
+}
+
+.bg-blue {
+    background-color: $bg-blue;
+}
+
+.bg-dark-blue {
+    background-color: $bg-dark-blue;
+}
+
+.title {
+    font-size: 24px;
+    font-weight: 400;
+    letter-spacing: 1px;
+}
+
+.content {
+    text-align: center;
+    font-size: 18px;
+    line-height: 38px;
+}
+</style>
--- src/components/mainComponents/inDepth.vue
+++ src/components/mainComponents/inDepth.vue
@@ -1,107 +1,36 @@
 <script>
-export default {}
+import database from '../../../db.json'
+import inDepthCard from './cards/inDepthcard.vue'
+export default {
+    components: {
+        inDepthCard
+    },
+    data() {
+        return {
+            database
+        }
+    },
+    computed: {
+        inDepthCards() {
+            return this.database.inDepthCards
+        }
+    }
+
+}
 </script>
 
 <template>
     <div class="row">
-        <div class="card col-4 white bg-light-blue">
-            <h3 class="title">Emergency Care</h3>
-            <img src="/wave-divider.png">
-            <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga vel provident ea facere
-                nesciunt blanditiis asperiores.</p>
-            <a href="#" class="btn btn-lb">Learn More</a>
-        </div>
-        <div class="card col-4 white bg-blue">
-            <h3 class="title">Expert Diagnostics</h3>
-            <img src="/wave-divider.png">
-            <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga vel provident ea facere
-                nesciunt blanditiis asperiores.</p>
-            <a href="#" class="btn btn-b">Learn More</a>
-        </div>
-        <div class="card col-4 white bg-dark-blue">
-            <h3 class="title">Superb Rehabilitation</h3>
-            <img src="/wave-divider.png">
-            <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga vel provident ea facere
-                nesciunt blanditiis asperiores.</p>
-            <a href="#" class="btn btn-db">Learn More</a>
-        </div>
+        <inDepthCard v-for="(card, index) in inDepthCards" :card="card" :key="index" />
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../../style/partials/variables' as *;
 
-button {
-    all: unset;
-}
 
-.card {
-    padding: 80px 30px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    row-gap: 30px;
-}
 
 .row {
     display: flex;
-}
-
-.col-4 {
-    flex-basis: calc(100% / 3);
-}
-
-.white {
-    color: white;
-}
-
-.btn {
-    padding: 16px 32px;
-    border: 3px solid white;
-    text-transform: uppercase;
-    font-size: 18px;
-    letter-spacing: 2px;
-}
-
-.btn-lb:hover {
-    border: 3px solid $bg-blue;
-    color: $bg-blue;
-    background-color: white;
-}
-
-.btn-b:hover {
-    border: 3px solid $bg-dark-blue;
-    color: $bg-dark-blue;
-    background-color: white;
-}
-
-.btn-db:hover {
-    border: 3px solid $light-blue;
-    color: $light-blue;
-    background-color: white;
-}
-
-.bg-light-blue {
-    background-color: $light-blue;
-}
-
-.bg-blue {
-    background-color: $bg-blue;
-}
-
-.bg-dark-blue {
-    background-color: $bg-dark-blue;
-}
-
-.title {
-    font-size: 24px;
-    font-weight: 400;
-    letter-spacing: 1px;
-}
-
-.content {
-    text-align: center;
-    font-size: 18px;
-    line-height: 38px;
 }
 </style>
